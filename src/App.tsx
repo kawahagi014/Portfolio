e93ef085ea46499be27624356e98be51ebdcd86d
Kawasaki Hironaga <kawasaki.cloud@gmail.com>
2022-06-19 19:49:11 +0900

    githubpages用の名前解決

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
-import { Router } from './components/router/Router';
-import theme from './theme/theme';
+import { Router } from "./components/router/Router";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
       <RecoilRoot>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Router />
         </BrowserRouter>
       </RecoilRoot>
